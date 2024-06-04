989cc4bf388746170f2a461ad2597cbb197cab6d
LENOVO <revz.ece@gmail.com>
2024-06-04 19:15:09 +0530

    Initial commit

---
+++ Ex_03062024/Lab001.py
@@ -0,0 +1,14 @@
+#This is single line comment
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+#Ctrl+D-entering duplicate values
+print(2+2)
+print(2/2)
+print(2-2)
+print(2*2)
+#Print command is used to print the values
+
+
